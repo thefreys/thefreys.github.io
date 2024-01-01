9542ebc19a2570b910ae57e23729d2013b1cc1ad
kristyfrey <kristyfrey@gmail.com>
2024-01-01 14:57:24 -0500

    temp sitemap

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,32 +2,53 @@ import * as mySitemapModule from './sitemap.js';
 import * as myContentModule from './content.js';
 import * as myMenuModule from './menu.js';
 
-
-
 var sitemap_items = [
   {"label": "Home", "href": "index.html?x=index"},
   {"label": "Recipes", "href": "index.html?x=recipes/", "children": [
     {"label": "Rainbow Cookies", "href": "index.html?x=recipes/rainbow-cookies"},
     {"label": "Tiramisu", "href": "index.html?x=recipes/tiramisu"},
+    {"label": "Almond Cake", "href": "index.html?x=recipes/almond-cake"},
+    {"label": "Chicken Meatballs", "href": "index.html?x=recipes/chicken-meatballs"},
+    {"label": "Ginger Snaps", "href": "index.html?x=recipes/ginger-snaps"},
+    {"label": "Mashed Potatoes", "href": "index.html?x=recipes/mashed-potatoes"},
     {"label": "Yeast Rolls", "href": "index.html?x=recipes/yeast-rolls"},
+  ]},
+  {"label": "Upcycling", "href": "index.html?x=upcycling/", "children": [
+    {"label": "Food Scraps", "href": "index.html?x=upcycling/food-scraps", "children": [
+      {"label": "Tea Bags", "href": "index.html?x=upcycling/food-scraps/tea-bags"},
+      {"label": "Coffee Grounds", "href": "index.html?x=upcycling/food-scraps/coffee-grounds"},
+    ]},
+    {"label": "Textiles", "href": "index.html?x=upcycling/textiles", "children": [
+      {"label": "T-Shirts", "href": "index.html?x=upcycling/textiles/t-shirts"},
+    ]},
   ]},
   {"label": "Web Design", "href": "index.html?x=web-design/", "children": [
     {"label": "Sitemaps", "href": "index.html?x=web-design/sitemaps"},
     {"label": "Hamburger Menu", "href": "index.html?x=web-design/hamburger"},
   ]},
-  {"label": "Creative Reuse", "href": "index.html?x=creative-reuse/", "children": [
-    {"label": "Food Scraps", "href": "index.html?x=creative-reuse/food-scraps", "children": [
-      {"label": "Tea Bags", "href": "index.html?x=creative-reuse/food-scraps/tea-bags"},
-      {"label": "Coffee Grounds", "href": "index.html?x=creative-reuse/food-scraps/coffee-grounds"},
-    ]},
-    {"label": "Textiles", "href": "index.html?x=creative-reuse/textiles", "children": [
-      {"label": "T-Shirts", "href": "index.html?x=creative-reuse/textiles/t-shirts"},
-    ]},
-  ]},
 ];
 
+var temp_items = [
+  'index.html?x=./asking-ai/technical/list-files-from-a-terminal/index',
+  'index.html?x=./recipes/desserts/almond-cake',
+  'index.html?x=./recipes/desserts/ginger-snaps',
+  'index.html?x=./recipes/desserts/rainbow-cookies',
+  'index.html?x=./recipes/desserts/tiramisu',
+  'index.html?x=./recipes/meats/chicken-meatballs',
+  'index.html?x=./recipes/sides/mashed-potatoes',
+  'index.html?x=./recipes/sides/yeast-rolls',
+  'index.html?x=./upcycling/food-scraps/coffee-grounds',
+  'index.html?x=./upcycling/food-scraps/tea-bags',
+  'index.html?x=./upcycling/textiles/t-shirts',
+  'index.html?x=./web-design/hamburger',
+  'index.html?x=./web-design/sitemaps',
+  ];
+  
+  
+
 var queryParams = myContentModule.getQueryParams();
-var request_path = 'index.html?x=' + queryParams['x'];
+//var request_path = 'index.html?x=' + queryParams['x'];
+var request_path = 'index.html?x=./' + queryParams['x'];
 //var arr = window.location.href.split("index.html?");
 if(request_path.endsWith('/index')){
   request_path = request_path.slice(0, -5);
@@ -51,8 +72,10 @@ function watchElementForChanges(elemId, callback) {
 
 watchElementForChanges('htmlContent', () => {
   console.log('htmlContent changed');
-  mySitemapModule.buildSiteMap(sitemap_items, document.getElementById("site-map"), null);
-  mySitemapModule.buildAreaMap(request_path, sitemap_items, document.getElementById("area-map"), null); 
+  //mySitemapModule.buildSiteMap(sitemap_items, document.getElementById("site-map"), null);
+  //mySitemapModule.buildAreaMap(request_path, sitemap_items, document.getElementById("area-map"), null); 
+  mySitemapModule.buildSiteMapX(temp_items, document.getElementById("site-map"), null);
+  mySitemapModule.buildAreaMapX(request_path, temp_items, document.getElementById("area-map"), null); 
 });
 
 watchElementForChanges('mdContent', () => {
@@ -64,5 +87,5 @@ watchElementForChanges('jsContent', () => {
 });
 
 //document.getElementById('menu-right').innerHTML = myMenuModule.buildMenuItems(bookmarks,'right', 'Bookmarks','menu-right', 0);
-document.getElementById('left-menu-form').style.display = 'block';
-document.getElementById('menu-left').innerHTML = myMenuModule.buildMenuItems(sitemap_items,'left', 'Menu','menu-left', 0);
+//document.getElementById('left-menu-form').style.display = 'block';
+//document.getElementById('menu-left').innerHTML = myMenuModule.buildMenuItems(sitemap_items,'left', 'Menu','menu-left', 0);
--- assets/js/sitemap.js
+++ assets/js/sitemap.js
@@ -43,3 +43,30 @@ export function buildAreaMap(request_path, items, parent) {
 
 
 
+
+
+
+    export function buildSiteMapX(items, parent) {    
+        var html = '';
+        if (parent === null){
+            return;
+        }
+        for (var i = 0; i < items.length; i++) { 
+            html = html + '<p><a href="'+items[i]+'"</a>'+items[i]+'</p>';
+        }    
+        parent.innerHTML = html; 
+    }
+    export function buildAreaMapX(request_path, items, parent) {  
+        if (parent === null){
+            return;
+        }
+        var html = '';  
+        for (var i = 0; i < items.length; i++) {  
+            var path = items[i];
+            console.log(path);
+            if(path.startsWith(request_path)){     
+                html = html + '<p><a href="'+items[i]+'"</a>'+items[i]+'</p>';
+            }
+        }     
+        parent.innerHTML = html; 
+    }
