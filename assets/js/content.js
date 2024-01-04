511f61a10f34a28026f42266a06bb190573da989
kristyfrey <kristyfrey@gmail.com>
2024-01-04 06:32:35 -0500

    content updates

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,15 +1,6 @@
-var config = {
-    contentPathPrefix: '../content/',
-};
-
-export function updateContentPathPrefix(newValue) {
-    config.contentPathPrefix = newValue;
-}
-
-export function getValue() {
-    return config;
-}
-
+var config = {};
+var page = {};
+var status = {isInitialized: false};
 
 export function getQueryParams() {
     var queryParams = {};
@@ -27,25 +18,89 @@ export function getQueryParams() {
     return queryParams;
 }
 
-export function loadContent() {
-    var queryParams = getQueryParams();
-    if (typeof queryParams['x'] === 'undefined') {
+export function init(pConfig) {
+    // Perform initialization tasks
+    config = pConfig;
+    page.queryParams = getQueryParams();       
+    if (typeof page.queryParams['x'] === 'undefined') {
         window.location.href = 'index.html?x=index';
         return;
     }
-    var path = queryParams['x'];
-
-    if (path.endsWith('/')){
-        window.location.href = 'index.html?x=' + path + 'index';
+    page.path = page.queryParams['x'];
+    if (page.path.endsWith('/')){
+        window.location.href = 'index.html?x=' + page.path + 'index';
         return;
     }
+    page.href = 'index.html?x=' + page.path; 
+    //page.filename = window.location.pathname.split('/').pop();
+    page.pathParts = page.path.split('/');
+    page.filename = page.pathParts.pop();
+    if(page.path == 'index'){
+        page.parentPath = null;
+    }
+    else {
+        page.parentPath = 'index.html?x=' + page.pathParts
+        .join('/');
+    }
 
-    var mdContentPath = config.contentPathPrefix +  path + '.md'; 
+    
+    if (typeof config['contentPathPrefix'] === 'undefined') {
+        config.contentPathPrefix = '../content/';
+    }
+    status.isInitialized = true;
+    console.log('Module initialized with pConfig:', pConfig);
+    console.log('Module config:', config);
+    console.log('Module status:', status);
+    console.log('Module page:', page);
+
+
+    var sitemapPath = config.contentPathPrefix + 'sitemap_items.json'; 
+    var mdContentPath = config.contentPathPrefix +  page.path + '.md'; 
     var mdContent404 = false;
-    var htmlContentPath = config.contentPathPrefix + path + '.html'; 
+    var htmlContentPath = config.contentPathPrefix + page.path + '.html'; 
     var htmlContent404 = false;
-    var jsContentPath = config.contentPathPrefix +  path + '.js'; 
+    var jsContentPath = config.contentPathPrefix +  page.path + '.js'; 
     var jsContent404 = false;
+    
+    var sitemapPromise = fetch(sitemapPath)
+        .then(response => {
+            if (!response.ok) {
+                if (response.status === 404) {
+                    console.log('sitemap_items.json not found: 404');
+                    return;
+                }
+                if (!response.ok) {
+                    throw new Error('Network response was not ok: ' + response.statusText);
+                }
+            }
+            return response.json(); 
+        })
+        .then(data => {
+            var items = data.items;
+            var sitemap = new Map();
+            var sitemapItems;
+            for (var i = 0; i < items.length; i++) {
+                var sitemapItem = {
+                    key: items[i].xpath,
+                    href: 'index.html?x=' + items[i].xpath,
+                    meta: items[i].meta
+                }
+                var path_parts = sitemapItem.key.split('/');
+                var last_element = path_parts.pop()
+                sitemap.set(items[i].xpath, sitemapItem);
+                //console.log(last_element);
+            }
+            //console.log(sitemap);
+            sitemap.forEach((value, key) => {
+                //console.log(key + '.href = ' + value.href);
+            });
+            config.sitemapItems = items;
+        })
+        .catch(error => {
+            console.log('There has been a problem with your fetch operation:', error);
+        });
+
+
        
     var mdPromise = fetch(mdContentPath)
         .then(response => {
@@ -124,15 +179,14 @@ export function loadContent() {
         });  
 
 
-    let promises = [mdPromise, htmlPromise, jsPromise];
+    let promises = [sitemapPromise, mdPromise, htmlPromise, jsPromise];
     
     Promise.all(promises)
         .then((results) => {
             console.log('All promises resolved:', results);
             if(mdContent404 && htmlContent404 && jsContent404){
-                if(path =='404'){
-                    document.getElementById('htmlContent').innerHTML = '404040404040404';
-                    //window.location.href = 'index.html?x=404';
+                if(page.path =='404'){
+                    document.getElementById('htmlContent').innerHTML = '404 page is missing';
                     return;
 
                 }
@@ -144,11 +198,49 @@ export function loadContent() {
             console.error('A promise failed:', error);
         });
 
-
-
 }
 
+export function buildSiteMap(items, parent) {    
+    if (parent === null){
+        return;
+    }
+    for (var i = 0; i < items.length; i++) {      
+        var li = document.createElement("li");     
+        if ("href" in items[i]) { 
+            var a = document.createElement("a");
+            a.textContent = items[i].label;    
+            a.setAttribute("href", items[i].href);
+            li.append(a);
+        }  
+        else{
+            li.textContent = items[i].label;    
+        }
+        if ("children" in items[i] && items[i].children.length > 0) {                  
+            var ul = document.createElement("ul");                
+            ul.setAttribute("id", parent.getAttribute("id") + "-" + i); 
+            buildSiteMap(items[i].children, ul);
+            li.append(ul);
+        }
+        parent.append(li);
+    }     
+}
 
+export function buildAreaMap(items, parent) {    
+    if (parent === null){
+        return;
+    }
+    for (var i = 0; i < items.length; i++) {  
+        if("href" in items[i] && items[i].href.startsWith(page.href) && items[i].href != page.href){     
+            buildSiteMap(items, parent);
+            return;
+        }
+        else {
+            if ("children" in items[i] && items[i].children.length > 0) {  
+                buildAreaMap(items[i].children, parent);
+            }
+        }
+    }     
+}
 
 
 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,6 @@
-import * as mySitemapModule from './sitemap.js';
 import * as myContentModule from './content.js';
-import * as myMenuModule from './menu.js';
 
-var sitemap_items = [
-  {"label": "Home", "href": "index.html?x=index"},
-  {"label": "Recipes", "href": "index.html?x=recipes/", "children": [
-    {"label": "Rainbow Cookies", "href": "index.html?x=recipes/rainbow-cookies"},
-    {"label": "Tiramisu", "href": "index.html?x=recipes/tiramisu"},
-    {"label": "Almond Cake", "href": "index.html?x=recipes/almond-cake"},
-    {"label": "Chicken Meatballs", "href": "index.html?x=recipes/chicken-meatballs"},
-    {"label": "Ginger Snaps", "href": "index.html?x=recipes/ginger-snaps"},
-    {"label": "Mashed Potatoes", "href": "index.html?x=recipes/mashed-potatoes"},
-    {"label": "Yeast Rolls", "href": "index.html?x=recipes/yeast-rolls"},
-  ]},
-  {"label": "Upcycling", "href": "index.html?x=upcycling/", "children": [
-    {"label": "Food Scraps", "href": "index.html?x=upcycling/food-scraps", "children": [
-      {"label": "Tea Bags", "href": "index.html?x=upcycling/food-scraps/tea-bags"},
-      {"label": "Coffee Grounds", "href": "index.html?x=upcycling/food-scraps/coffee-grounds"},
-    ]},
-    {"label": "Textiles", "href": "index.html?x=upcycling/textiles", "children": [
-      {"label": "T-Shirts", "href": "index.html?x=upcycling/textiles/t-shirts"},
-    ]},
-  ]},
-  {"label": "Web Design", "href": "index.html?x=web-design/", "children": [
-    {"label": "Sitemaps", "href": "index.html?x=web-design/sitemaps"},
-    {"label": "Hamburger Menu", "href": "index.html?x=web-design/hamburger"},
-  ]},
-];
-
-var temp_items = [
-  'index.html?x=./asking-ai/technical/list-files-from-a-terminal/index',
-  'index.html?x=./recipes/desserts/almond-cake',
-  'index.html?x=./recipes/desserts/ginger-snaps',
-  'index.html?x=./recipes/desserts/rainbow-cookies',
-  'index.html?x=./recipes/desserts/tiramisu',
-  'index.html?x=./recipes/meats/chicken-meatballs',
-  'index.html?x=./recipes/sides/mashed-potatoes',
-  'index.html?x=./recipes/sides/yeast-rolls',
-  'index.html?x=./upcycling/food-scraps/coffee-grounds',
-  'index.html?x=./upcycling/food-scraps/tea-bags',
-  'index.html?x=./upcycling/textiles/t-shirts',
-  'index.html?x=./web-design/hamburger',
-  'index.html?x=./web-design/sitemaps',
-  ];
-  
-  
-
-var queryParams = myContentModule.getQueryParams();
-//var request_path = 'index.html?x=' + queryParams['x'];
-var request_path = 'index.html?x=./' + queryParams['x'];
-//var arr = window.location.href.split("index.html?");
-if(request_path.endsWith('/index')){
-  request_path = request_path.slice(0, -5);
-}  
-console.log(request_path);
-myContentModule.loadContent();
+myContentModule.init({});
 
 function watchElementForChanges(elemId, callback) {
   const observer = new MutationObserver((mutationsList) => {
@@ -74,8 +20,8 @@ watchElementForChanges('htmlContent', () => {
   console.log('htmlContent changed');
   //mySitemapModule.buildSiteMap(sitemap_items, document.getElementById("site-map"), null);
   //mySitemapModule.buildAreaMap(request_path, sitemap_items, document.getElementById("area-map"), null); 
-  mySitemapModule.buildSiteMapX(temp_items, document.getElementById("site-map"), null);
-  mySitemapModule.buildAreaMapX(request_path, temp_items, document.getElementById("area-map"), null); 
+  //mySitemapModule.buildSiteMapX(temp_items, document.getElementById("site-map"), null);
+  //mySitemapModule.buildAreaMapX(request_path, temp_items, document.getElementById("area-map"), null); 
 });
 
 watchElementForChanges('mdContent', () => {
