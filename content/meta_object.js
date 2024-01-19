a2793e40ade8309a4beb80167cf993be1c62039f
kristyfrey <kristyfrey@gmail.com>
2024-01-19 07:26:50 -0500

    new meta object

--- content/meta_object.js
+++ content/meta_object.js
@@ -9,3 +9,143 @@ title: "Bard said:",
 hasMarkdown: true,
 },
 'asking-ai/crafty/how-do-you-make-t-shirt-yarn/bing':{
+title: "bing said:",
+hasMarkdown: true,
+},
+'asking-ai/crafty/how-do-you-make-t-shirt-yarn/chatgpt':{
+title: "ChatGPT said:",
+hasMarkdown: true,
+},
+'asking-ai/crafty/how-do-you-make-t-shirt-yarn/index':{
+title: "How do you make t-shirt yarn?",
+},
+'asking-ai/technical/list-files-from-a-terminal/bing':{
+title: "bing said:",
+hasMarkdown: true,
+},
+'asking-ai/technical/list-files-from-a-terminal/index':{
+title: "How do you list files from a terminal",
+},
+'asking-ai/technical/most-common-term-for-writing-computer-code/bard':{
+title: "Bard said:",
+hasMarkdown: true,
+},
+'asking-ai/technical/most-common-term-for-writing-computer-code/bing':{
+title: "bing said:",
+hasMarkdown: true,
+},
+'asking-ai/technical/most-common-term-for-writing-computer-code/chatgpt':{
+title: "ChatGPT said:",
+hasMarkdown: true,
+},
+'asking-ai/technical/most-common-term-for-writing-computer-code/index':{
+title: "What is the most common term for writing computer code?",
+},
+'blogs/kristy/2024/01/01-planting-seeds-using-my-morning-tea-bags':{
+hasMarkdown: true,
+},
+'blogs/kristy/2024/01/14-feeding-leftover-bacon-fat-to-birds':{
+hasMarkdown: true,
+},
+'blogs/kristy/2024/01/18-asking-ai':{
+hasMarkdown: true,
+},
+'blogs/kristy/2024/01/19-asking-ai':{
+hasMarkdown: true,
+},
+'coding/frameworks/bootstrap/cheatsheet-v5.3':{
+hasHtml: true,
+},
+'coding/languages/markdown/markdown-cheat-sheet':{
+hasMarkdown: true,
+},
+'coding/web-design/hamburger':{
+title: "Hamburger Menus",
+hasMarkdown: true,
+},
+'coding/web-design/index':{
+hasHtml: true,
+},
+'coding/web-design/sitemaps':{
+title: "Sitemaps",
+hasMarkdown: true,
+},
+'index':{
+title: "Welcome",
+hasMarkdown: true,
+},
+'recipes/desserts/almond-cake':{
+title: "Almond cake",
+hasMarkdown: true,
+},
+'recipes/desserts/date-balls':{
+title: "Date Balls",
+hasMarkdown: true,
+},
+'recipes/desserts/ginger-snaps':{
+title: "Ginger snaps",
+hasMarkdown: true,
+},
+'recipes/desserts/index':{
+hasHtml: true,
+},
+'recipes/desserts/rainbow-cookies':{
+title: "Rainbow Cookies",
+hasMarkdown: true,
+},
+'recipes/desserts/tiramisu':{
+title: "Tiramisu",
+hasMarkdown: true,
+},
+'recipes/index':{
+title: "Recipes",
+hasHtml: true,
+},
+'recipes/meats/chicken-meatballs':{
+title: "Chicken meatballs",
+hasMarkdown: true,
+},
+'recipes/meats/index':{
+hasHtml: true,
+},
+'recipes/sides/index':{
+hasHtml: true,
+},
+'recipes/sides/macaroni-and-cheese-1-pot-version':{
+title: "One-Pot Macaroni and Cheese Recipe",
+hasMarkdown: true,
+},
+'recipes/sides/mashed-potatoes':{
+title: "Mashed potatoes",
+hasMarkdown: true,
+},
+'recipes/sides/yeast-rolls':{
+title: "Yeast Rolls",
+hasMarkdown: true,
+},
+'sitemap':{
+title: "Sitemap",
+hasHtml: true,
+},
+'upcycling/food-scraps/coffee-grounds':{
+title: "Reusing Coffee Grounds",
+hasMarkdown: true,
+},
+'upcycling/food-scraps/index':{
+hasHtml: true,
+},
+'upcycling/food-scraps/tea-bags':{
+title: "Reusing Tea Bags",
+hasMarkdown: true,
+},
+'upcycling/index':{
+hasHtml: true,
+},
+'upcycling/textiles/index':{
+hasHtml: true,
+},
+'upcycling/textiles/t-shirts':{
+title: "Upcycling T-shirts",
+hasMarkdown: true,
+},
+};
